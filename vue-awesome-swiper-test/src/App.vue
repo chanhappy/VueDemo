403800c3cf3ec0f316010161065a747c97c71a9a
zhangchanjuan <zhangchanjuan@agree.com.cn>
2023-03-31 13:56:02 +0800

    提交vue2.0的基础上，vue-awesome-swiper@3.1.3滑动组件的测试程序

---
+++ vue-awesome-swiper-test/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/1.jpg" /> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <AwesomeSwiperTest />
+  </div>
+</template>
+
+<script>
+// import HelloWorld from "./components/HelloWorld.vue";
+import AwesomeSwiperTest from './components/AwesomeSwiperTest.vue'
+
+export default {
+  name: "App",
+  components: {
+    // HelloWorld,
+    AwesomeSwiperTest
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-awesome-swiper-test/src/components/AwesomeSwiperTest.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <swiper :options="swiperOption" class="swiper">
+      <swiper-slide v-for="(item, index) in images" :key="index">
+        <img :src="item.url" alt="" />
+      </swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+<script>
+import { swiper, swiperSlide } from "vue-awesome-swiper";
+import "swiper/dist/css/swiper.css";
+
+export default {
+  name: "vue-awesome-swiper-test",
+  components: {
+    swiper,
+    swiperSlide,
+  },
+  data() {
+    return {
+      swiperOption: {
+        autoplay: {
+            delay: 1000, // 设置每屏滑动的时间间隔为3秒
+        },
+        // speed: 1000, // 滑动速度，即slider自动滑动开始到结束的时间（单位ms）
+        loop: true, // 是否循环播放
+        autoplayDisableOnInteraction: false, // 手动滑动后是否禁止autoplay
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+      },
+      images: [
+        { url: "./../assets/1.jpg" },
+        { url: "./../assets/2.jpg" },
+        { url: "./../assets/3.jpg" },
+      ],
+    };
+  },
+};
+</script>
+<style>
+.swiper-container {
+  width: 500px;
+  height: 300px;
+}
+
+img {
+  width: 500px;
+  height: 300px;
+  background: red;
+}
+</style>
